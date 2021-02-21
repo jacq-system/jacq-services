302b49c32eea6c546250309a2a8855b139c1f80e
aragon112358 <joschach@ap4net.at>
2021-02-21 11:32:49 +0100

    added samples file

---
+++ commonNames/references/scientificName/inc/variables.sample.php
@@ -0,0 +1,12 @@
+<?php
+$_CONFIG['DATABASES']['HERBARINPUT'] = array(
+    "host" => "localhost",
+    "db"   => "herbarinput",
+    "user" => "user",
+    "pass" => "password"
+);
+$_CONFIG['displayErrorDetails'] = false;
+$_CONFIG['JACQ_INPUT_SERVICES'] = "http://url-to-input-service/";
+$_CONFIG['APIKEY'] = "api-key";
+
+$_CONFIG['classifications_license'] = 'CC-BY-SA';
