80409a32321b43b1231b30065cb26a6533fd0457
d.roepert <d.roepert@bgbm.org>
2023-04-04 12:22:35 +0200

    added function to get manifest data from post curl jacq-serlet service

--- rest/classes/IiifMapper.php
+++ rest/classes/IiifMapper.php
@@ -32,10 +32,11 @@ public function getManifestUri(int $specimenID): array
  */
 public function getManifest(int $specimenID, string $currentUri)
 {
-    $specimen = $this->db->query("SELECT s.specimen_ID, iiif.manifest_backend
+    $specimen = $this->db->query("SELECT s.specimen_ID,s.herbnummer, iiif.manifest_backend, iiif.manifest_uri, img.imgserver_Prot, img.imgserver_IP, img.iiif_dir, img.key
                                   FROM tbl_specimens s
-                                   LEFT JOIN tbl_management_collections mc        ON mc.collectionID = s.collectionID
-                                   LEFT JOIN herbar_pictures.iiif_definition iiif ON iiif.source_id_fk = mc.source_id
+                                  LEFT JOIN tbl_management_collections mc        ON mc.collectionID = s.collectionID
+                                  LEFT JOIN herbar_pictures.iiif_definition iiif ON iiif.source_id_fk = mc.source_id
+                                  LEFT JOIN herbarinput.tbl_img_definition img ON img.source_id_fk = mc.source_id
                                   WHERE specimen_ID = '$specimenID'")
                          ->fetch_assoc();
     if (!$specimen['manifest_backend']) {
@@ -45,9 +46,127 @@ public function getManifest(int $specimenID, string $currentUri)
 
         $result = array();
         if ($manifestBackend) {
-            $curl = curl_init($manifestBackend);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $curl_response = curl_exec($curl);
+            if(substr($manifestBackend,0,5) == 'POST:') {
+                $url = substr($manifestBackend,5); //tbl_img_definition.imgserver_IP
+                $urlmanifestpre = $this->makeURI($specimen['specimen_ID'], $this->parser($specimen['manifest_uri'])); //iiif_definition.manifest_uri
+                $urlmanifestpost = '/manifest.json';
+                $urliiif = $specimen['imgserver_Prot'].'://'.$specimen['imgserver_IP'].$specimen['iiif_dir'].'/';
+                $key = $specimen['key'];
+                $filename = $this->getPictureData($specimenID); //woher? 'dr_045258'
+                //$herbariumid = $this->makeURI($specimen['specimen_ID'], [['text'=>'stableIdentifier:last', 'token' => true]]);//'dr045258';
+                $file_type = 'image/jp2'; //eventuell aus Ergebnis request
+
+                $data = array(
+                    'id' => '1',
+                    'method' => 'listResourcesWithMetadata',
+                    'params' => array(
+                        $key,
+                        array(
+                            $filename,
+                            $filename . "_%",
+                            $filename . "A",
+                            $filename . "B",
+                            "tab_" . $filename,
+                            "obs_" . $filename,
+                            "tab_" . $filename . "_%",
+                            "obs_" . $filename . "_%"
+                        )
+                    )
+                );
+
+                $data_string = json_encode($data);
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_POST, true);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                        'Content-Type: application/json',
+                        'Content-Length: ' . strlen($data_string))
+                );
+
+                $curl_response = curl_exec($curl);
+
+                $obj = json_decode($curl_response, TRUE);
+
+                curl_close($curl);
+
+                //$result = array();
+
+                $context = array('http://iiif.io/api/presentation/2/context.json',
+                    'http://www.w3.org/ns/anno.jsonld'
+                );
+                $result['@context'] = $context ;
+                $result['@id']      = $urlmanifestpre.$urlmanifestpost;
+                $result['@type']      = 'sc:Manifest';
+                $result['label']      = $specimenID;
+                $canvases = array();
+                for($i=0; $i<count($obj['result']); $i++) {
+                    $canvases[] =  array('@id' => $urlmanifestpre.'/c/'.$specimenID,
+                        '@type' => 'sc:Canvas',
+                        '@label' =>  $obj['result'][$i]["identifier"],
+                        'height' =>  $obj['result'][$i]["height"],
+                        'width' =>  $obj['result'][$i]["width"],
+                        'images' => array('@id' => $urlmanifestpre.'/i/'.$specimenID,
+                            '@type' => 'oa:Annotation',
+                            'motivation' => 'sc:painting',
+                            'on' => $urlmanifestpre.'/c/'.$specimenID,
+                            'resource' => array('@id' => $urliiif.str_replace('/','!',substr($obj['result'][$i]["path"],1)),
+                                '@type' => 'dctypes:Image',
+                                'format' => $file_type,
+                                'height' => $obj['result'][$i]["height"],
+                                'width' => $obj['result'][$i]["width"],
+                                '@service' => array('@context' => 'http://iiif.io/api/image/2/context.json',
+                                    '@id' => $urliiif.str_replace('/','!',substr($obj['result'][$i]["path"],1)),
+                                    'profile' => 'http://iiif.io/api/image/2/level2.json',
+                                    'protocol' => 'http://iiif.io/api/image'
+                                ),
+                            ),
+                        ),
+                    );
+
+                };
+                $sequences = array('@id' => $urlmanifestpre.'/c/'.$specimenID,
+                    '@type' => 'sc:Sequence',
+                    'canvases' => $canvases,
+                    'label' => 'Current order',
+                    'viewingDirection' => 'left-to-right'
+                );
+                $result['@sequences']      = $sequences;
+
+                $result['thumbnail']      = array('@id' => $urliiif.str_replace('/','!',substr($obj['result'][0]["path"],1)).'/full/400,/0/default.jpg',
+                    '@type' => 'dctypes:Image',
+                    'format' => 'image/jpeg',
+                    '@service' => array('@context' => 'http://iiif.io/api/image/2/context.json',
+                        '@id' => $urliiif.str_replace('/','!',substr($obj['result'][0]["path"],1)),
+                        'profile' => 'http://iiif.io/api/image/2/level2.json',
+                        'protocol' => 'http://iiif.io/api/image'
+                    ),
+                );
+
+                    $specimen = new SpecimenMapper($this->db, $specimen['specimen_ID']);
+
+                    $result['@id']         = $currentUri;  // to point at ourselves
+                    $result['description'] = $specimen->getDescription();
+                    $result['label']       = $specimen->getLabel();
+                    $result['attribution'] = $specimen->getAttribution();
+                    $result['logo']        = array('@id' => $specimen->getLogoURI());
+                    $rdfLink               = array('@id'     => $specimen->getStableIdentifier(),
+                        'label'   => 'RDF',
+                        'format'  => 'application/rdf+xml',
+                        'profile' => 'https://cetafidentifiers.biowikifarm.net/wiki/CSPP');
+                    if (empty($result['seeAlso'])) {
+                        $result['seeAlso']   = array($rdfLink);
+                    } else {
+                        $result['seeAlso'][] = $rdfLink;
+                    }
+                    $result['metadata']    = $this->getMetadataWithValues($specimen, (isset($result['metadata'])) ? $result['metadata'] : array());
+
+            }else{
+                $curl = curl_init($manifestBackend);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                $curl_response = curl_exec($curl);
+
             if ($curl_response !== false) {
                 $result = json_decode($curl_response, true);
                 $specimen = new SpecimenMapper($this->db, $specimen['specimen_ID']);
@@ -69,6 +188,7 @@ public function getManifest(int $specimenID, string $currentUri)
                 $result['metadata']    = $this->getMetadataWithValues($specimen, (isset($result['metadata'])) ? $result['metadata'] : array());
             }
             curl_close($curl);
+            };
         }
 
         return $result;
@@ -233,5 +353,159 @@ private function getMetadataWithValues(SpecimenMapper $specimen, array $metadata
     }
     return $result;
 }
+private function getPictureData($specimenID)
+    {
+        $result = $this->db->query("SELECT id.`imgserver_Prot`, id.`imgserver_IP`, id.`imgserver_type`, id.`img_service_directory`, id.`is_djatoka`, id.`HerbNummerNrDigits`, id.`key`,
+                   mc.`coll_short_prj`, mc.`source_id`, mc.`collectionID`, mc.`picture_filename`,
+                   s.`HerbNummer`, s.`Bemerkungen`
+            FROM `tbl_specimens` s
+             LEFT JOIN `tbl_management_collections` mc ON mc.`collectionID` = s.`collectionID`
+             LEFT JOIN `tbl_img_definition` id ON id.`source_id_fk` = mc.`source_id`
+            WHERE s.`specimen_ID` = '" . intval($specimenID) . "'")
+             ->fetch_assoc();
+        // Fetch information for this image
+        if ($result) {
+           $url = ((!empty($result['imgserver_Prot'])) ? $result['imgserver_Prot'] : "http") . '://'
+                . $result['imgserver_IP']
+                . (($result['img_service_directory']) ? '/' . $result['img_service_directory'] . '/' : '/');
+
+            // Remove hyphens
+            $HerbNummer = str_replace('-', '', $result['HerbNummer']);
+
+
+            // Construct clean filename
+            if ($result['imgserver_type'] == 'bgbm') {
+                // Remove spaces for B HerbNumber
+                $HerbNummer = ($result['HerbNummer']) ? $result['HerbNummer'] : ('JACQID' . $specimenID);
+                $HerbNummer = str_replace(' ', '', $HerbNummer);
+                $filename = sprintf($HerbNummer);
+                $key = $result['key'];
+            } elseif ($result['imgserver_type'] == 'baku') {       // depricated
+                $html = $result['Bemerkungen'];
+// create new ImageQuery object
+                $query = new ImageQuery();
+
+// fetch image uris
+                try {
+                    $uris = $query->fetchUris($html);
+                } catch (Exception $e) {
+                    echo 'an error occurred: ', $e->getMessage(), "\n";
+                    die();
+                }
+
+// do something with uris
+                foreach ($uris as $uriSubset) {
+                    $newHtmlCode = '<a href="' . $uriSubset["image"] . '" target="_blank"><img src="' . $uriSubset["preview"] . '"/></a>';
+                }
+
+                $url = $uriSubset["base"];
+#$url .= ($row['img_service_directory']) ? '/' . $row['img_service_directory'] . '/' : '';
+                if (substr($url, -1) != '/') {
+                    $url .= '/';  // to ensure that $url ends with a slash
+                }
+                $filename = sprintf($uriSubset["filename"]);
+                $originalFilename = sprintf($uriSubset["thumb"]);
+                $key = sprintf($uriSubset["html"]);
+            } else {
+                if ($result['collectionID'] == 90 || $result['collectionID'] == 92 || $result['collectionID'] == 123) { // w-krypt needs special treatment
+                    /* TODO
+                     * specimens of w-krypt are currently under transition from the old numbering system (w-krypt_1990-1234567) to the new
+                     * numbering system (w_1234567). During this time, new HerbNumbers are given to the specimens and the entries
+                     * in tbl_specimens are changed accordingly.
+                     * So, this script should first look for pictures, named after the new system before searching for pictures, named after the old system
+                     * When the transition is finished, this code-part (the whole elseif-block) should be removed
+                     * Johannes Schachner, 25.9.2021
+                     */
+                    $filename = sprintf("w_%0" . $result['HerbNummerNrDigits'] . ".0f", $HerbNummer);
+                    try {  // ask the picture server for a picture with the new filename
+                        $service = new \JsonRPC\Client($url . 'jacq-servlet/ImageServer');
+                        $pics = $service->execute('listResources',
+                            [
+                                $result['key'],
+                                [
+                                    $filename,
+                                    $filename . "_%",
+                                    $filename . "A",
+                                    $filename . "B",
+                                    "tab_" . $filename,
+                                    "obs_" . $filename,
+                                    "tab_" . $filename . "_%",
+                                    "obs_" . $filename . "_%"
+                                ]
+                            ]);
+                    } catch (Exception $e) {
+                        $pics = array();  // something has gone wrong, so no picture can be found anyway
+                    }
+                    if (count($pics) == 0) {  // nothing found, so use the old filename
+                        $filename = sprintf("w-krypt_%0" . $result['HerbNummerNrDigits'] . ".0f", $HerbNummer);
+                    }
+                } elseif (!empty($result['picture_filename'])) {   // special treatment for this collection is necessary
+                    $parts = $this-> parser($result['picture_filename']);
+                    $filename = '';
+                    foreach ($parts as $part) {
+                        if ($part['token']) {
+                            $tokenParts = explode(':', $part['text']);
+                            $token = $tokenParts[0];
+                            switch ($token) {
+                                case 'coll_short_prj':                                      // use contents of coll_short_prj
+                                    $filename .= $result['coll_short_prj'];
+                                    break;
+                                case 'HerbNummer':                                          // use HerbNummer with removed hyphens, options are :num and :reformat
+                                    if (in_array('num', $tokenParts)) {                     // ignore text with digits within, only use the last number
+                                        if (preg_match("/\d+$/", $HerbNummer, $matches)) {  // there is a number at the tail of HerbNummer
+                                            $number = $matches[0];
+                                        } else {                                            // HerbNummer ends with text
+                                            $number = 0;
+                                        }
+                                    } else {
+                                        $number = $HerbNummer;                              // use the complete HerbNummer
+                                    }
+                                    if (in_array("reformat", $tokenParts)) {                // correct the number of digits with leading zeros
+                                        $filename .= sprintf("%0" . $result['HerbNummerNrDigits'] . ".0f", $number);
+                                    } else {                                                // use it as it is
+                                        $filename .= $number;
+                                    }
+                                    break;
+                            }
+                        } else {
+                            $filename .= $part['text'];
+                        }
+                    }
+                } else {    // standard filename, would be "<coll_short_prj>_<HerbNummer:reformat>"
+                    $filename = sprintf("%s_%0" . $result['HerbNummerNrDigits'] . ".0f", $result['coll_short_prj'], $HerbNummer);
+                }
+                $key = $result['key'];
+            }
+
+// Set original file-name if we didn't pass one (required for djatoka)
+// (required for pictures with suffixes)
+            //if (!$originalFilename) {
+             //   $originalFilename = $filename;
+            //}
+
+            //return array(
+            //    'url' => $url,
+            //    'requestFileName' => $filename,
+            //    //'originalFilename' => $originalFilename,
+            //    'filename' => $filename,
+            //   'specimenID' => $specimenID,
+            //    'is_djatoka' => $result['is_djatoka'],
+            //    'imgserver_type' => $result['imgserver_type'],
+            //    'key' => $key
+            //);
+            return $filename;
+        } else {
+            return array(
+                'url' => null,
+                'requestFileName' => null,
+                //'originalFilename' => null,
+                'filename' => null,
+                'specimenID' => null,
+                'is_djatoka' => null,
+                'imgserver_type' => null,
+                'key' => null
+            );
+        }
+    }
 
 }
