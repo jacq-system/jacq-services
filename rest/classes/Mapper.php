596e567b18b4c9947d7587d8663d13963477d7aa
aragon112358 <joschach@ap4net.at>
2024-07-13 10:35:50 +0200

    find serviceBaseUrl automatically

--- rest/classes/Mapper.php
+++ rest/classes/Mapper.php
@@ -6,12 +6,24 @@ abstract class Mapper
      * @var mysqli
      */
     protected mysqli $db;
-    private string $serviceBaseUrl = "https://services.jacq.org/jacq-services/rest";
+    private string $serviceBaseUrl; // will be something like https://services.jacq.org/jacq-services/rest
 
     public function __construct(mysqli $db)
     {
         $this->db = $db;
-//        $this->serviceBaseUrl = 'https://' . $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['SCRIPT_NAME']));
+
+        // should tls be used?
+        if (   (($_SERVER['REQUEST_SCHEME'] ?? '') == 'https')
+            || (($_SERVER['HTTPS'] ?? '') == 'on')
+            || (($_SERVER['SERVER_PORT'] ?? '') == '443')
+            || (($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '') == 'https')
+            || (($_SERVER['SERVER_NAME'] ?? '') == 'services.jacq.org')
+           ) {
+            $server_request_scheme = 'https://';
+        } else {
+            $server_request_scheme = 'http://';
+        }
+        $this->serviceBaseUrl = $server_request_scheme . $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['SCRIPT_NAME']));
     }
 
     /**
